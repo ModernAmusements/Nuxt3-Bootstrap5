4c54b01a2181cb515fd888edcb6570b06c884f39
shady <shady@epunks.de>
2022-06-17 17:00:54 +0200

    test footer mixins not working

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,5 +4,10 @@ import {defineNuxtConfig} from 'nuxt3'
 export default defineNuxtConfig({
     css: [
         '~/assets/scss/main.scss'
-    ]
+    ],
+    components: {
+        dirs: [
+          '~/components'
+        ]
+      }
 })
